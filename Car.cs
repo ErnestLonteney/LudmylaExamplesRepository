b8ba452d5d40a19fe601e5e70d75df06ba8c78b4
Nesterchuk Volodymyr <ampersant@ukr.net>
2025-12-13 23:08:31 +0100

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,82 @@
+﻿namespace CarExample;
+
+class Car
+{
+    #region СТАН ОБЄКТА (ПОЛЯ)
+
+
+    // Коли потрібна валідаційна логіка це виглядає так
+    private string mark;
+    public string Mark
+    {
+        get { return this.mark; }
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                this.mark = value;
+            }
+        }
+    }
+
+    // Це буде створено за лаштунками, ми ж моржемо користуватись Влас
+    //public string GetMark()
+    //{
+    //    return this.mark.Replace("a", "*");
+    //}
+
+    //public void SetMark(string value)
+    //{
+    //    if (!string.IsNullOrEmpty(value))
+    //       this.mark = value;
+    //}
+
+
+    // Коли нам не потрібна валідаційна логіка використовуємо авто-властивість
+    // В майбутньому вона може бути розширена у звичайну властивість
+    public string Color { get; set; } 
+
+
+    private string model;
+    private DateTime year;
+    private string vin;
+    private double engineCapacity;
+    private double currentSpeed;
+
+    #endregion
+
+
+    #region Коструювання о'бєкта (КОНСТРУКТОРИ)
+    // Конструктор приймає 2 параметри
+    public Car(string mark, string model)
+    {
+        this.mark = mark;
+        this.model = model;
+    }
+
+    // Конструктор приймає 3 параметри
+    public Car(string mark, string model, DateTime year)
+        :this(mark, model)
+    {           
+        this.year = year;
+    }
+
+    #endregion
+
+    #region ПОВЕДІНКА ОБ'ЄКТА (МЕТОДИ)
+    public void Start()
+    {
+        Console.WriteLine("Car ius started");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Car is stopped");
+    }
+
+    public void Accelerate(byte delta)
+    {
+        this.currentSpeed += delta;
+    }
+    #endregion
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CarExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // створення об'єкта класу Car
+            var myCar = new Car("Toyota", "Corolla");
+          //  myCar. = "Red";
+
+            myCar.Mark = "BMW";    
+            Console.WriteLine(myCar.Mark);
+
+            myCar.Color = "Blue";
+            Console.WriteLine(myCar.Color);
+
+            // створення об'єкта класу Car
+            var anotherCar = new Car("Honda", "Civic");
+            // створення об'єкта класу Car
+            var newCar = new Car("Renault", "Megan");
+            // створення об'єкта класу Car
+            var car = new Car("Ford", "Focus", new DateTime(1, 1, 2024));
+
+           car.Start();
+
+           car.Accelerate(50);
+           car.Accelerate(30);
+
+           car.Stop();
+
+        }
+    }
+}
